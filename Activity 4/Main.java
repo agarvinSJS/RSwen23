cd4263464d1778213110c3f65f1d6d41d8728c0f
RPS1222 <rps1222yt@gmail.com>
2021-09-23 10:15:11 -0500

    Edited Main.java

--- Activity 4/Main.java
+++ Activity 4/Main.java
@@ -1,6 +1,7 @@
-public class  Main {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Rhyder");
         System.out.println("Swen");
+        System.out.println("rswen23@sjs.org")
     }
 }
